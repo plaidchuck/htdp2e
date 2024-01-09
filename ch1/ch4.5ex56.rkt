9a25ba8014efa8aad5bb60d31c3d69090b6bc11f
plaidchuck <142672627+plaidchuck@users.noreply.github.com>
2024-01-08 21:15:40 -0500

    Add files via upload

--- ch1/ch4.5ex56.rkt
+++ ch1/ch4.5ex56.rkt
@@ -11,7 +11,7 @@
 
 (define HEIGHT 300) ; distances in pixels 
 (define WIDTH  100)
-(define YDELTA 3)
+(define YDELTA 6)
  
 (define BACKG  (empty-scene WIDTH HEIGHT))
 (define ROCKET (rectangle 5 30 "solid" "red"))
@@ -30,7 +30,7 @@
 ;                  10 (* 3/4 WIDTH)
 ;                  (place-rocket (- HEIGHT CENTER))))
  
-;(check-expect
+; (check-expect
 ; (show 53)
 ; (place-rocket (- 53 CENTER)))
 
@@ -42,8 +42,7 @@
                   10 (* 3/4 WIDTH)
                   (place-rocket (- HEIGHT CENTER)))]
     [(>= x 0)
-     (place-rocket(- x CENTER))]
-     [else (place-rocket (- HEIGHT CENTER))]))
+     (place-rocket(- x CENTER))]))
 
 
 ; LRCD -> Image
@@ -55,12 +54,12 @@
 ; LRCD KeyEvent -> LRCD
 ; starts the countdown when space bar is pressed, 
 ; if the rocket is still resting 
-(check-expect (launch "resting" " ") -3)
-(check-expect (launch "resting" "a") "resting")
-(check-expect (launch -3 " ") -3)
-(check-expect (launch -1 " ") -1)
-(check-expect (launch 33 " ") 33)
-(check-expect (launch 33 "a") 33)
+; (check-expect (launch "resting" " ") -3)
+; (check-expect (launch "resting" "a") "resting")
+; (check-expect (launch -3 " ") -3)
+; (check-expect (launch -1 " ") -1)
+; (check-expect (launch 33 " ") 33)
+; (check-expect (launch 33 "a") 33)
 
 (define (launch x ke)
   (cond
@@ -70,14 +69,29 @@
 
  
 ; LRCD -> LRCD
-; raises the rocket by YDELTA,
-;  if it is moving already 
+; raises the rocket by YDELTA if it is moving already 
+; (check-expect (fly "resting") "resting")
+; (check-expect (fly -3) -2)
+; (check-expect (fly -2) -1)
+; (check-expect (fly -1) HEIGHT)
+; (check-expect (fly 10) (- 10 YDELTA))
+; (check-expect (fly 22) (- 22 YDELTA))
+ 
 (define (fly x)
-  x)
+  (cond
+    [(string? x) x]
+    [(<= -3 x -1) (if (= x -1) HEIGHT (+ x 1))]
+    [(>= x 0) (- x YDELTA)]))
 
 ; LRCD -> LRCD
-(define (main1 s)
+; Stops program when rocket state dissappears into upper part of canvas
+(define (off-screen x)
+  (and (number? x)(= x 0)))
+
+
+(define (main2 s)
   (big-bang s
     [to-draw show]
-    [on-key launch]))
-
+    [on-tick fly 0.3]
+    [on-key launch]
+    [stop-when off-screen]))
